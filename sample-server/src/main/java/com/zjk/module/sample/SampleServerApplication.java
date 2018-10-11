a02ae961874f2a409b613a6a8ee6aa2ee3369d11
bbjohn <295322187@qq.com>
2018-10-11 16:05:23 +0800

    init

--- sample-server/src/main/java/com/zjk/module/sample/SampleServerApplication.java
+++ sample-server/src/main/java/com/zjk/module/sample/SampleServerApplication.java
@@ -2,6 +2,7 @@ package com.zjk.module.sample;
 
 import com.zjk.module.common.authorization.server.annotation.EnableCommonAuthorizationServerConfiguration;
 import com.zjk.module.common.base.annotation.EnableCommonBaseConfiguration;
+import com.zjk.module.common.redis.annotation.EnableCommonRedisConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 //@EnableCommonAuthorizationServerConfiguration
 @EnableCommonBaseConfiguration
+@EnableCommonRedisConfiguration
 public class SampleServerApplication {
 
 	public static void main(String[] args) {
