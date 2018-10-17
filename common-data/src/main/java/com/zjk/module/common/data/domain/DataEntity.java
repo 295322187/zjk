fdd586b80435439903e826ae967da3f40c9fccad
bbjohn <295322187@qq.com>
2018-10-17 11:09:56 +0800

    init

--- common-data/src/main/java/com/zjk/module/common/data/domain/DataEntity.java
+++ common-data/src/main/java/com/zjk/module/common/data/domain/DataEntity.java
@@ -11,7 +11,7 @@ import java.util.Date;
 public class DataEntity extends BaseEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	@Column(columnDefinition = "timestamp", updatable = false)
